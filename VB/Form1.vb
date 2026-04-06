39b557bc1f8e94b505bc17019f64a66bb6b04855
DevExpressExampleBot <scmaintenance@devexpress.com>
2026-04-06 11:47:19 +0400

    autogenerate VB

--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -15,38 +15,37 @@ Imports DevExpress.XtraRichEdit
 Imports DevExpress.XtraBars.Docking2010.Views
 
 Namespace ChildStandaloneForm
-	Partial Public Class Form1
-		Inherits XtraForm
 
-		Public Sub New()
-			InitializeComponent()
-			tabbedView1.FloatingDocumentContainer = DevExpress.XtraBars.Docking2010.Views.FloatingDocumentContainer.DocumentsHost
-		End Sub
+    Public Partial Class Form1
+        Inherits XtraForm
 
-		Private docs As New List(Of BaseDocument)()
-		Private Sub tabbedView1_QueryControl(ByVal sender As Object, ByVal e As DevExpress.XtraBars.Docking2010.Views.QueryControlEventArgs) Handles tabbedView1.QueryControl
-			Dim wb As New MyBrowser()
-			wb.Dock = DockStyle.Fill
-			AddHandler wb.TitleChanged, AddressOf wb_TitleChanged
-			docs.Add(e.Document)
-			e.Control = wb
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            tabbedView1.FloatingDocumentContainer = DevExpress.XtraBars.Docking2010.Views.FloatingDocumentContainer.DocumentsHost
+        End Sub
 
-		Private Function FindDocument(ByVal documentControl As Object) As Document
-			For Each doc As Document In docs
-				If doc.Control Is documentControl Then
-					Return doc
-				End If
-			Next doc
-			Return Nothing
-		End Function
+        Private docs As List(Of BaseDocument) = New List(Of BaseDocument)()
 
-		Private Sub wb_TitleChanged(ByVal sender As Object, ByVal e As EventArgs)
-			tabbedView1.ActiveDocument.Caption = (TryCast(tabbedView1.ActiveDocument.Control, MyBrowser)).Title
-			Dim activeFloatDoc As Document = FindDocument(sender) ' for floating documents
-			If activeFloatDoc IsNot Nothing Then
-				activeFloatDoc.Caption = (TryCast(sender, MyBrowser)).Title
-			End If
-		End Sub
-	End Class
+        Private Sub tabbedView1_QueryControl(ByVal sender As Object, ByVal e As DevExpress.XtraBars.Docking2010.Views.QueryControlEventArgs)
+            Dim wb As MyBrowser = New MyBrowser()
+            wb.Dock = DockStyle.Fill
+            wb.TitleChanged += AddressOf wb_TitleChanged
+            docs.Add(e.Document)
+            e.Control = wb
+        End Sub
+
+        Private Function FindDocument(ByVal documentControl As Object) As Document
+            For Each doc As Document In docs
+                If doc.Control Is documentControl Then Return doc
+            Next
+
+            Return Nothing
+        End Function
+
+        Private Sub wb_TitleChanged(ByVal sender As Object, ByVal e As EventArgs)
+            Me.tabbedView1.ActiveDocument.Caption = TryCast(Me.tabbedView1.ActiveDocument.Control, ChildStandaloneForm.MyBrowser).Title
+            Dim activeFloatDoc As Document = Me.FindDocument(sender) ' for floating documents
+            If activeFloatDoc IsNot Nothing Then activeFloatDoc.Caption = TryCast(sender, ChildStandaloneForm.MyBrowser).Title
+        End Sub
+    End Class
 End Namespace
--- VB/MyBrowser.vb
+++ VB/MyBrowser.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Drawing
@@ -8,105 +8,104 @@ Imports System.Text
 Imports System.Windows.Forms
 
 Namespace ChildStandaloneForm
-	Partial Public Class MyBrowser
-		Inherits UserControl
 
-		Private Const topIndent As Integer = 30
-		Private Const startPage As String = "http://www.google.com"
-		Public Sub New()
-			InitializeComponent()
-			SubscribeWebBrowserEvents()
-			LoadNewDocument(startPage)
-			AddHandler buttonEdit1.KeyDown, AddressOf buttonEdit1_KeyDown
-			AddHandler Me.Resize, AddressOf MyBrowser_Resize
-			AddHandler Me.Disposed, AddressOf MyBrowser_Disposed
-		End Sub
+    Public Partial Class MyBrowser
+        Inherits UserControl
 
-		Private Sub MyBrowser_Disposed(ByVal sender As Object, ByVal e As EventArgs)
-			webBrowser1.Dispose()
-		End Sub
+        Const topIndent As Integer = 30
 
-		Private Sub MyBrowser_Resize(ByVal sender As Object, ByVal e As EventArgs)
-			webBrowser1.Width = Me.Width - 6
-			webBrowser1.Height = Me.Height - topIndent
-		End Sub
+        Const startPage As String = "http://www.google.com"
 
-		Private Sub buttonEdit1_KeyDown(ByVal sender As Object, ByVal e As KeyEventArgs)
-			If e.KeyCode = Keys.Enter Then
-				LoadNewDocument(buttonEdit1.Text)
-			End If
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+            SubscribeWebBrowserEvents()
+            Me.LoadNewDocument(startPage)
+            buttonEdit1.KeyDown += New KeyEventHandler(AddressOf buttonEdit1_KeyDown)
+            Me.Resize += New EventHandler(AddressOf MyBrowser_Resize)
+            Me.Disposed += New EventHandler(AddressOf MyBrowser_Disposed)
+        End Sub
 
-		Private Sub SubscribeWebBrowserEvents()
-			AddHandler webBrowser1.DocumentTitleChanged, AddressOf webBrowser1_DocumentTitleChanged
-			AddHandler webBrowser1.CanGoBackChanged, AddressOf webBrowser1_CanGoBackChanged
-			AddHandler webBrowser1.CanGoForwardChanged, AddressOf webBrowser1_CanGoForwardChanged
-		End Sub
+        Private Sub MyBrowser_Disposed(ByVal sender As Object, ByVal e As EventArgs)
+            webBrowser1.Dispose()
+        End Sub
 
-		Private Sub webBrowser1_CanGoForwardChanged(ByVal sender As Object, ByVal e As EventArgs)
-			btnForward.Enabled = webBrowser1.CanGoForward
-		End Sub
+        Private Sub MyBrowser_Resize(ByVal sender As Object, ByVal e As EventArgs)
+            webBrowser1.Width = Me.Width - 6
+            webBrowser1.Height = Me.Height - topIndent
+        End Sub
 
-		Private Sub webBrowser1_CanGoBackChanged(ByVal sender As Object, ByVal e As EventArgs)
-			btnBack.Enabled = webBrowser1.CanGoBack
-		End Sub
-		Private Sub webBrowser1_DocumentTitleChanged(ByVal sender As Object, ByVal e As EventArgs)
-			SetTitle(webBrowser1.DocumentTitle)
-			buttonEdit1.Text = Convert.ToString(webBrowser1.Url)
-		End Sub
+        Private Sub buttonEdit1_KeyDown(ByVal sender As Object, ByVal e As KeyEventArgs)
+            If e.KeyCode Is Keys.Enter Then LoadNewDocument(buttonEdit1.Text)
+        End Sub
 
-		' Fields...
-		Private _Title As String
+        Private Sub SubscribeWebBrowserEvents()
+            webBrowser1.DocumentTitleChanged += New EventHandler(AddressOf webBrowser1_DocumentTitleChanged)
+            webBrowser1.CanGoBackChanged += New EventHandler(AddressOf webBrowser1_CanGoBackChanged)
+            webBrowser1.CanGoForwardChanged += New EventHandler(AddressOf webBrowser1_CanGoForwardChanged)
+        End Sub
 
-		Public ReadOnly Property Title As String
-			Get
-				Return _Title
-			End Get
-		End Property
+        Private Sub webBrowser1_CanGoForwardChanged(ByVal sender As Object, ByVal e As EventArgs)
+            btnForward.Enabled = webBrowser1.CanGoForward
+        End Sub
 
-		Public Event TitleChanged As EventHandler
+        Private Sub webBrowser1_CanGoBackChanged(ByVal sender As Object, ByVal e As EventArgs)
+            btnBack.Enabled = webBrowser1.CanGoBack
+        End Sub
 
-		Protected Overridable Sub OnTitleChangedEvent()
-			RaiseEvent TitleChanged(Me, EventArgs.Empty)
-		End Sub
+        Private Sub webBrowser1_DocumentTitleChanged(ByVal sender As Object, ByVal e As EventArgs)
+            SetTitle(webBrowser1.DocumentTitle)
+            buttonEdit1.Text = Convert.ToString(webBrowser1.Url)
+        End Sub
 
-		Private Sub SetTitle(ByVal s As String)
-			_Title = s
-			OnTitleChangedEvent()
-		End Sub
+        ' Fields...
+        Private _Title As String
 
-		Private Sub LoadNewDocument(ByVal address As String)
-			If String.IsNullOrEmpty(address) Then
-				Return
-			End If
-			If address.Equals("about:blank") Then
-				Return
-			End If
-			If Not address.StartsWith("http://") AndAlso Not address.StartsWith("https://") Then
-				address = "http://" & address
-			End If
-			Try
-				webBrowser1.Navigate(New Uri(address))
-				buttonEdit1.Text = address
-			Catch e1 As System.UriFormatException
-				Return
-			End Try
-		End Sub
+        Public ReadOnly Property Title As String
+            Get
+                Return _Title
+            End Get
+        End Property
 
-		Private Sub buttonEdit1_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.ButtonPressedEventArgs) Handles buttonEdit1.ButtonClick
-			LoadNewDocument(buttonEdit1.Text)
-		End Sub
+        Public Event TitleChanged As EventHandler
 
-		Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnBack.Click
-			webBrowser1.GoBack()
-		End Sub
+        Protected Overridable Sub OnTitleChangedEvent()
+            RaiseEvent TitleChangedEvent(Me, EventArgs.Empty)
+        End Sub
 
-		Private Sub simpleButton2_Click(ByVal sender As Object, ByVal e As EventArgs) Handles btnForward.Click
-			webBrowser1.GoForward()
-		End Sub
+        Private Sub SetTitle(ByVal s As String)
+            _Title = s
+            OnTitleChangedEvent()
+        End Sub
 
-		Private Sub simpleButton1_Click_1(ByVal sender As Object, ByVal e As EventArgs) Handles simpleButton1.Click
-			LoadNewDocument(buttonEdit1.Text)
-		End Sub
-	End Class
+        Private Sub LoadNewDocument(ByVal address As String)
+            If [String].IsNullOrEmpty(address) Then Return
+            If address.Equals("about:blank") Then Return
+            If Not address.StartsWith("http://") AndAlso Not address.StartsWith("https://") Then
+                address = "http://" & address
+            End If
+
+            Try
+                webBrowser1.Navigate(New Uri(address))
+                buttonEdit1.Text = address
+            Catch __unusedUriFormatException1__ As System.UriFormatException
+                Return
+            End Try
+        End Sub
+
+        Private Sub buttonEdit1_ButtonClick(ByVal sender As Object, ByVal e As DevExpress.XtraEditors.Controls.ButtonPressedEventArgs)
+            LoadNewDocument(buttonEdit1.Text)
+        End Sub
+
+        Private Sub simpleButton1_Click(ByVal sender As Object, ByVal e As EventArgs)
+            webBrowser1.GoBack()
+        End Sub
+
+        Private Sub simpleButton2_Click(ByVal sender As Object, ByVal e As EventArgs)
+            webBrowser1.GoForward()
+        End Sub
+
+        Private Sub simpleButton1_Click_1(ByVal sender As Object, ByVal e As EventArgs)
+            LoadNewDocument(buttonEdit1.Text)
+        End Sub
+    End Class
 End Namespace
