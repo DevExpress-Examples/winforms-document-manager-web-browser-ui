2526609c1dbb55ff7005c1448af4e8f4dbd72398
DevExpressExampleBot <scmaintenance@devexpress.com>
2026-04-06 13:25:13 +0400

    Source auto update [skip ci]

--- VB/Program.vb
+++ VB/Program.vb
@@ -1,19 +1,19 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
+Imports System
 Imports System.Windows.Forms
 
 Namespace ChildStandaloneForm
-	Friend Module Program
-		''' <summary>
-		''' The main entry point for the application.
-		''' </summary>
-		<STAThread>
-		Sub Main()
-			Application.EnableVisualStyles()
-			Application.SetCompatibleTextRenderingDefault(False)
-			DevExpress.Skins.SkinManager.EnableFormSkins()
-			Application.Run(New Form1())
-		End Sub
-	End Module
+
+    Friend Module Program
+
+        ''' <summary>
+        ''' The main entry point for the application.
+        ''' </summary>
+        <STAThread>
+        Sub Main()
+            Call Application.EnableVisualStyles()
+            Application.SetCompatibleTextRenderingDefault(False)
+            DevExpress.Skins.SkinManager.EnableFormSkins()
+            Call Application.Run(New Form1())
+        End Sub
+    End Module
 End Namespace
