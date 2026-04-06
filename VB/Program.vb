38c657875c6e63b3dd4bb97ce689eca30f9bf758
Svetlana Sarmina <svetlana.sarmina@devexpress.com>
2026-04-06 11:44:28 +0400

    Add VB

---
+++ VB/Program.vb
@@ -0,0 +1,19 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Windows.Forms
+
+Namespace ChildStandaloneForm
+	Friend Module Program
+		''' <summary>
+		''' The main entry point for the application.
+		''' </summary>
+		<STAThread>
+		Sub Main()
+			Application.EnableVisualStyles()
+			Application.SetCompatibleTextRenderingDefault(False)
+			DevExpress.Skins.SkinManager.EnableFormSkins()
+			Application.Run(New Form1())
+		End Sub
+	End Module
+End Namespace
